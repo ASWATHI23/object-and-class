923cc473618902bcb7b68bf1447606af00a241a2
ASWATHI23 <110898037+ASWATHI23@users.noreply.github.com>
2022-08-25 14:23:53 +0530

    first commit

---
+++ class.py
@@ -0,0 +1,9 @@
+class Aswathi:
+    attr1="Aswathi"
+    attr2="21"
+    def student(self):
+        print("my name is",self.attr1)
+        print("my age is",self.attr2)
+Name=Aswathi()
+print(Name.attr1)
+Name.student()
